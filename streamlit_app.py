8b1e485ae697b5a7316f2f7b56f32594629abc2a
Alexey Nesterenko <comconadin@gmail.com>
2026-06-09 22:15:15 +0000

    chore: add simple auth try

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
-st.title("🎈 Testing G.Auth 🎈")
+cur_title = "🎈 Testing G.Auth 🎈"
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.set_page_config(page_title="Blank app with Google auth")
+
+if not st.user.is_logged_in:
+    st.title(cur_title)
+    st.write("Please sign in.")
+    st.button("Log in with Google", on_click=st.login)
+else:
+    st.title(cur_title)
+    st.success(f"Signed in as {st.user.name}")
+    st.write(f"Email: {st.user.email}")
+    st.button("Log out", on_click=st.logout)
